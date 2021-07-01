f79725f10d1759cc81c18c6d62ad12ed8fdb6093
framinggraphics <matteo.mastromoro@gmail.com>
2021-07-01 23:46:16 +0200

    Fixes on footer and some pages

--- js/randomtext.js
+++ js/randomtext.js
@@ -1,8 +1,8 @@
 function randomtext() {
     var randomtxt=[
         'Annie makes the unreal seem hyperreal, and the real more supremely alive and utterly magical.<br>&nbsp;<br>Jeff Goldblum about Annie Atkins',
-        'Marco De Matteo',
-        'Karen TenEyck', ];
+        'Cit<br>&nbsp;<br>Marco De Matteo',
+        'Cit<br>&nbsp;<br>Karen TenEyck', ];
     return randomtxt[Math.floor((Math.random() * 2.99))];
 }
 
