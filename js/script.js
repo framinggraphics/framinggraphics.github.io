60e05583d700007e5db2697f613906f4d31cccb2
framinggraphics <matteo.mastromoro@gmail.com>
2021-07-02 00:07:14 +0200

    Fixes

--- js/script.js
+++ js/script.js
@@ -740,8 +740,6 @@ $(window).scroll(function() {
 
 */
 
-
-
     function UncheckAll(){ 
         var w = document.getElementsByTagName('input'); 
         for(var i = 0; i < w.length; i++){ 
@@ -750,38 +748,6 @@ $(window).scroll(function() {
           }
         }
     }
-
-
-    var isAdvancedUpload = function() {
-        var div = document.createElement('div');
-        return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-      }();
-
-
-      var $form = $('.box');
-    if (isAdvancedUpload) {
-    $form.addClass('has-advanced-upload');
-    }
-
-    if (isAdvancedUpload) {
-
-        var droppedFiles = false;
-      
-        $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-        })
-        .on('dragover dragenter', function() {
-          $form.addClass('is-dragover');
-        })
-        .on('dragleave dragend drop', function() {
-          $form.removeClass('is-dragover');
-        })
-        .on('drop', function(e) {
-          droppedFiles = e.originalEvent.dataTransfer.files;
-        });
-      
-      }
 
 UncheckAll();
 createclasses();
