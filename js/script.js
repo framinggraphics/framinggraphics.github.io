b02b7dbfbde3491147f9e4feb24b08cf37204ef0
framinggraphics <matteo.mastromoro@gmail.com>
2021-07-02 17:39:00 +0200

    fixes to IronMan page

--- js/script.js
+++ js/script.js
@@ -749,6 +749,15 @@ $(window).scroll(function() {
         }
     }
 
+    function showlegend() {
+        var x = document.getElementById("legend");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      } 
+
 UncheckAll();
 createclasses();
 creategallery();
