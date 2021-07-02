142c4fa87d8793af67a1097ec18150168d5f769d
framinggraphics <matteo.mastromoro@gmail.com>
2021-07-02 12:28:07 +0200

    Fixed landing

--- js/randomtext.js
+++ js/randomtext.js
@@ -1,8 +1,8 @@
 function randomtext() {
     var randomtxt=[
         'Annie makes the unreal seem hyperreal, and the real more supremely alive and utterly magical.<br>&nbsp;<br>Jeff Goldblum about Annie Atkins',
-        'Cit<br>&nbsp;<br>Marco De Matteo',
-        'Cit<br>&nbsp;<br>Karen TenEyck', ];
+        'Nostro compito è quello di accompagnare l’immersione dello spettatore, descrivere il personaggio anche in base ai quadri da lui appesi nella sua stanza, o a cosa ha poggiato sul tavolo.<br>&nbsp;<br>Marco De Matteo',
+        'We make street signs and maps that let you know where you are going.<br>&nbsp;<br>Karen TenEyck', ];
     return randomtxt[Math.floor((Math.random() * 2.99))];
 }
 
